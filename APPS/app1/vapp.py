12aa586d12482271bb6f4d6991c73c6b9121095f
WangJun <445212619@qq.com>
2019-07-09 15:43:27 +0800

    first commit

---
+++ APPS/app1/vapp.py
@@ -0,0 +1,28 @@
+
+from threading import *
+import time
+import sys
+
+class vMain(object):
+    def __init__(self):
+        print "__init__"
+
+    def RegisterInterface(self):
+        def td_func():
+            while self.isRun:
+                print "vAppName:"+sys.vAppName
+                time.sleep(3)    
+        
+        self.isRun=True
+        self.td=Thread(target=td_func)
+        print 'RegisterInterface'
+
+    def Initialize(self):
+        self.td.start()
+        print 'Initialize'
+
+    def FreeData(self):
+        self.isRun=False
+        self.td.join()
+        self.td=None
+        print 'FreeData'
---
+++ README.md
@@ -0,0 +1,1 @@
+# VirtualPyEnv
